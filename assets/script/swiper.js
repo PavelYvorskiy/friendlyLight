3945515300debf85bae06996bcaba8e8e3c0c007
Pavel <pavelyvorskiy82@gmail.com>
2022-12-13 06:15:59 +0200

    master

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -1,62 +1,7 @@
 const swiper = new Swiper(".mySwiper", {
   navigation: {
-    nextEl: ".next",
-    prevEl: ".prev",
-  },
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-const swiper1 = new Swiper(".mySwiper1", {
-  navigation: {
-    nextEl: ".next1",
-    prevEl: ".prev1",
-  },
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-const swiper2 = new Swiper(".mySwiper2", {
-  navigation: {
-    nextEl: ".next2",
-    prevEl: ".prev2",
-  },
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-const swiper3 = new Swiper(".mySwiper3", {
-  navigation: {
-    nextEl: ".next3",
-    prevEl: ".prev3",
-  },
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-const swiper4 = new Swiper(".mySwiper4", {
-  navigation: {
-    nextEl: ".next4",
-    prevEl: ".prev4",
-  },
-  effect: "fade",
-  speed: 500,
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-const swiper5 = new Swiper(".mySwiper5", {
-  navigation: {
-    nextEl: ".next5",
-    prevEl: ".prev5",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   effect: "fade",
   speed: 500,
